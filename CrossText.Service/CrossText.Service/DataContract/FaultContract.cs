34644bef78126391327d8c0ba2cfe1689f515de7
famstutz <florian@amstutz.nu>
2010-11-27 15:43:41 +0100

    initial checking including a well-formed .gitignore-file

---
+++ CrossText.Service/CrossText.Service/DataContract/FaultContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace CrossText.Service.DataContract
+{
+    public class FaultContract
+    {
+
+        public FaultContract()
+        { }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string StackTrace { get; set; }
+
+        [DataMember]
+        public string Source { get; set; }
+    }
+}
