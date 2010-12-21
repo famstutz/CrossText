b218d6e76ff2ba79fae33b39a1f6be039ab50832
famstutz <florian@amstutz.nu>
2010-12-21 14:42:18 +0100

    Created a TeletextHelper

---
+++ service/CrossText.Service/Helpers/TeletextHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrossText.Service.Helpers
+{
+    public class TeletextHelper
+    {
+        public static string FormatTeletextUrl(string baseUrl, int siteNumber, int subSiteNumber)
+        {
+            return string.Format(baseUrl, siteNumber, subSiteNumber);
+        }
+
+        public static string FormatTeletextUrl(int siteNumber, int subSiteNumber)
+        {
+            return FormatTeletextUrl(ConfigurationHelper.TeletextBaseUrl, siteNumber, subSiteNumber);
+        }
+    }
+}
